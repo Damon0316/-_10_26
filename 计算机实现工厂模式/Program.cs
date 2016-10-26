e8ebf53ade1f8281e97237970bdddc814698d218
璇 杨 <329540516@qq.com>
2016-10-26 13:23:20 +0800

    简单工厂模式

---
+++ 计算机实现工厂模式/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 计算机实现工厂模式
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("please input first number");
+            string strNumberA = Console.ReadLine();
+            Console.WriteLine("please input +、-、*、/");
+            string operation = Console.ReadLine();
+            Console.WriteLine("please input second number");
+            string strNumberB = Console.ReadLine();
+            Operation oper = null;
+            oper = SimplyFactory.createOperate(operation);
+            oper.numberA = Convert.ToDouble(strNumberA);
+            oper.numberB = Convert.ToDouble(strNumberB);            
+            Console.WriteLine("结果是:{0}",oper.GetResult().ToString());
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 计算机实现工厂模式/SimplyFactory.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 计算机实现工厂模式
+{
+
+    class Operation
+    {
+         public double numberA { get; set; }
+         public double numberB { get; set; }
+        public virtual double GetResult()
+        {
+            double result = 0;
+            return result;
+        }
+    }
+
+    class operationAdd : Operation
+    {
+        public override double GetResult()
+        {
+            double result = 0;
+            result = numberA + numberB;
+            return result;
+        }
+    }
+
+    class operationSub : Operation
+    {
+        public double GetResult()
+        {
+            return numberA - numberB;
+        }
+    }
+
+    class operationMul : Operation
+    {
+        public double GetResult()
+        {
+            return numberA * numberB;
+        }
+    }
+
+    class operationDiv : Operation
+    {
+        public double GetResult()
+        {
+            return numberA / numberB;
+        }
+    }
+
+    class SimplyFactory
+    {
+        public static Operation createOperate(string operate)
+        {
+            Operation oper = null;
+            switch (operate)
+            {
+                case "+":
+                    {
+                        oper = new operationAdd();
+                        break;
+                    }
+                case "-":
+                    {
+                        oper = new operationSub();
+                        break;
+                    }
+                case "*":
+                    {
+                        oper = new operationAdd();
+                        break;
+                    }
+                case "/":
+                    {
+                        oper = new operationSub();
+                        break;
+                    }
+            }
+            return oper;
+        }
+    }
+}
